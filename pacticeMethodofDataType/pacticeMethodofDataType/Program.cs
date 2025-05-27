1ffeb1d0d2aecc9439abf05fe5a19ca9b1e0c1a3
Naman <sharmanaman9561@gmail.com>
2025-05-27 14:00:05 +0530

    data type & its method practice also Practice Array methods #1846/part 4 Work with variable data in C# console applications (Get started with C#, Part 4) - Training | Microsoft Learn

---
+++ pacticeMethodofDataType/pacticeMethodofDataType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacticeMethodofDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "102";
+            int result = 0;
+            if (int.TryParse(value, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+
+            string value1 = "bad";
+            int result1 = 0;
+            if (int.TryParse(value1, out result1))
+            {
+                Console.WriteLine($"Measurement: {result1}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+
+            if (result1 > 0)
+                Console.WriteLine($"Measurement (w/ offset): {50 + result1}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ practiceSort&ReverseMethod/practiceSort&ReverseMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceSort_ReverseMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pallets = { "B14", "A11", "B12", "A13" };
+
+            Console.WriteLine("Sorted...");
+            Array.Sort(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Reversed...");
+            Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+            Console.ReadLine();
+        }
+    }
+}

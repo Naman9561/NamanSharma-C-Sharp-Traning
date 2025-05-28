ff261c1824dced89cb0c6a1bd7736adf63b9f105
Naman <sharmanaman9561@gmail.com>
2025-05-28 11:41:15 +0530

    #1847/Part 5/ Create methods in C# console applications (Get started with C#, Part 5) - Training | Microsoft Learn Practice passbyvalue &passbyrefrence

---
+++ passByValue&passByRefrence/passByValue&passByRefrence/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passByValue_passByRefrence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test Pass By Value
+
+            int a = 3;
+            int b = 4;
+            int c = 0;
+
+            Console.WriteLine($"global statement: {a} x {b} = {c}");
+            Multiply(a, b, c);
+            void Multiply(int d, int e, int f)
+            {
+                f = d * e;
+                Console.WriteLine($"inside Multiply method: {d} x {e} = {f}");
+            }
+
+            Console.WriteLine("************************");
+            //Test Pass By Refrence
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            PrintArray(array);
+            Clear(array);
+            PrintArray(array);
+
+            void PrintArray(int[] arrays)
+            {
+                foreach (int x in arrays)
+                {
+                    Console.Write($"{x} ");
+                }
+                Console.WriteLine();
+            }
+
+            void Clear(int[] arrays)
+            {
+                for (int i = 0; i < arrays.Length; i++)
+                {
+                    arrays[i] = 0;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

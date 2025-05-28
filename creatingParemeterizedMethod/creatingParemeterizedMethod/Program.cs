fc8eaa60608b05a91edacc3d788d20735d8343a7
Naman <sharmanaman9561@gmail.com>
2025-05-28 10:58:53 +0530

    #1847/part 5/Create methods in C# console applications (Get started with C#, Part 5) - Training | Microsoft Learn Practice Parameterized Method & tested variable scope

---
+++ creatingParemeterizedMethod/creatingParemeterizedMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingParemeterizedMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] schedule = { 800, 1200, 1600, 2000 };
+            int diff = 0;
+            DisplayAdjustedTimes(schedule, 6, -6);
+
+            void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+            {
+                if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+                {
+                    Console.WriteLine("Invalid GMT");
+                }
+                else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+                {
+                    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+                }
+                else
+                {
+                    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+                }
+                for (int i = 0; i < times.Length; i++)
+                {
+                    int newTime = ((times[i] + diff)) % 2400;
+                    Console.WriteLine($"{times[i]} -> {newTime}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ testVariableScope/testVariableScope/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testVariableScope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+            DisplayStudents(students);
+            DisplayStudents(new string[] { "Robert", "Vanya" });
+
+            void DisplayStudents(string[] studentss)
+            {
+                foreach (string student in studentss)
+                {
+                    Console.Write($"{student}, ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("*************************");
+            PrintCircleArea(12);
+            PrintCircleCircumference(12);
+            void PrintCircleArea(int radius)
+            {
+                double pi = 3.14159;
+                double area = pi * (radius * radius);
+                Console.WriteLine($"Area = {area}");
+            }
+            void PrintCircleCircumference(int radius)
+            {
+                double pi = 3.14159;
+                double circumference = 2 * pi * radius;
+                Console.WriteLine($"Circumference = {circumference}");
+            }
+            Console.ReadLine();
+        }
+    }
+}

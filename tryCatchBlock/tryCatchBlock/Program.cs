5e16823513750991d9738fcf2b88aab9b117726e
Naman <sharmanaman9561@gmail.com>
2025-05-28 16:18:26 +0530

    #1848/part 6/Debug C# console applications (Get started with C#, Part 6) - Training | Microsoft Learn practiced try catch block

---
+++ tryCatchBlock/tryCatchBlock/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process1();
+            }
+            catch
+            {
+                Console.WriteLine("An exception has occurred");
+            }
+
+            Console.WriteLine("Exit program");
+
+            void Process1()
+            {
+                try
+                {
+                    WriteMessage();
+                }
+                catch
+                {
+                    Console.WriteLine("Exception caught in Process1");
+                }
+
+            }
+
+            void WriteMessage()
+            {
+                double float1 = 3000.0;
+                double float2 = 0.0;
+                int number1 = 3000;
+                int number2 = 0;
+
+                Console.WriteLine(float1 / float2);
+                Console.WriteLine(number1 / number2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tryCatchBlock/tryCatchBlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tryCatchBlock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tryCatchBlock")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("327509ca-15fe-4d58-ac60-b0a1e4bf3630")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

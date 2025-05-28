7b0a90cfe5edde16844572525fc72c7622129aa8
Naman <sharmanaman9561@gmail.com>
2025-05-28 13:49:08 +0530

    #1847/part 5/Create methods in C# console applications (Get started with C#, Part 5) - Training | Microsoft Learn practice of return type methods

---
+++ arrayReturnTypeMethod/arrayReturnTypeMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayReturnTypeMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 30;
+            int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+            int[,] results = TwoCoins(coins, target);
+
+            if (results.Length == 0)
+            {
+                Console.WriteLine("No two coins make change");
+            }
+            else
+            {
+                Console.WriteLine("Change found at positions:");
+                for (int i = 0; i < results.GetLength(0); i++)
+                {
+                    if (results[i, 0] == -1)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"{results[i, 0]},{results[i, 1]}");
+                }
+            }
+
+            int[,] TwoCoins(int[] coinss, int targets)
+            {
+                int[,] result ={ { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+                int count = 0;
+
+                for (int curr = 0; curr < coinss.Length; curr++)
+                {
+                    for (int next = curr + 1; next < coinss.Length; next++)
+                    {
+                        if (coinss[curr] + coinss[next] == targets)
+                        {
+                            result[count, 0] = curr;
+                            result[count, 1] = next;
+                            count++;
+                        }
+                        if (count == result.GetLength(0))
+                        {
+                            return result;
+                        }
+                    }
+                }
+                return (count == 0) ? new int[0, 0] : result;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ practiceReturnTypeMethod/practiceReturnTypeMethod/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceReturnTypeMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            double minimumSpend = 30.00;
+
+            double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+            double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                total += GetDiscountedPrice(i);
+            }
+
+            total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+            Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+            double GetDiscountedPrice(int itemIndex)
+            {
+                return items[itemIndex] * (1 - discounts[itemIndex]);
+            }
+
+            bool TotalMeetsMinimum()
+            {
+                return total >= minimumSpend;
+            }
+
+            string FormatDecimal(double input)
+            {
+                return input.ToString().Substring(0, 5);
+            }
+            Console.ReadLine();
+        }
+    }
+}

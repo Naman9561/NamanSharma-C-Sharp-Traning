1ecef7128aa403386f51daa1fc7caa8a41d510f4
Naman <sharmanaman9561@gmail.com>
2025-06-02 12:09:50 +0530

    #1858/Part 1/https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/object-oriented/ Diffrence Between Class & Struct

---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            // Other properties, methods, events...
+        }
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Leopold", 6);
+            Console.WriteLine($"person1 Name = {person1.Name} Age = {person1.Age}");
+
+            // Declare new person, assign person1 to it.
+            Person person2 = person1;
+
+            // Change the name of person2, and person1 also changes.
+            person2.Name = "Molly";
+            person2.Age = 16;
+
+            Console.WriteLine($"person2 Name = {person2.Name} Age = {person2.Age}");
+            Console.WriteLine($"person1 Name = {person1.Name} Age = {person1.Age}");
+
+            Person person3 = new Person("Naman",23);
+            Console.WriteLine($"person3 Name = {person3.Name} Age = {person3.Age}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StructPractice/StructPractice/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPractice
+{
+    internal class Program
+    {
+        public struct Person
+        {
+            public string Name;
+            public int Age;
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Create  struct instance and initialize by using "new".
+            // Memory is allocated on thread stack.
+            Person p1 = new Person("Alex", 9);
+            Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
+
+            // Create  new struct object. Note that  struct can be initialized
+            // without using "new".
+            Person p2 = p1;
+
+            // Assign values to p2 members.
+            p2.Name = "Spencer";
+            p2.Age = 7;
+            Console.WriteLine($"p2 Name = {p2.Name} Age = {p2.Age}");
+
+            // p1 values remain unchanged because p2 is  copy.
+            Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
+
+            Person p3 = new Person("Naman", 23);
+            Console.WriteLine($"p3 Name = {p3.Name} Age = {p3.Age}");
+
+            Console.ReadLine();
+        }
+    }
+}

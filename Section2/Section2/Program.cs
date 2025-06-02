950e82abc4d2aba4ccda09d7a269e62c9d2409fd
Naman <sharmanaman9561@gmail.com>
2025-06-02 15:40:38 +0530

    Part 2 Section 3 Practice Code

---
+++ Section2/Section2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var giftCard = new GiftCardAccount("gift card", 100, 50);
+            giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive coffee");
+            giftCard.MakeWithdrawal(50, DateTime.Now, "buy groceries");
+
+            giftCard.PerformMonthEndTransactions();
+            giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spending money");
+            Console.WriteLine("GIFT CARD ACCOUNT");
+            Console.WriteLine(giftCard.GetAccountHistory());
+
+            var savings = new InterestEarningAccount("savings account", 10000);
+            savings.MakeDeposit(750, DateTime.Now, "save some money");
+            savings.MakeDeposit(1250, DateTime.Now, "Add more savings");
+            savings.MakeWithdrawal(250, DateTime.Now, "pay bills");
+            savings.PerformMonthEndTransactions();
+            Console.WriteLine("SAVINGS ACCOUNT");
+            Console.WriteLine(savings.GetAccountHistory());
+
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine("LINE OF CREDIT ACCOUNT");
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine(); // Keeps console window open
+        }
+    }
+}

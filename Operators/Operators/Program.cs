7c2418a3b3ca23a0d1d78f6accb6556f4ecb1e80
Naman <sharmanaman9561@gmail.com>
2025-06-03 14:49:58 +0530

    Used the && and || operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a dividend: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a divisor: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            if ((divisor != 0) && (dividend / divisor) is var result)
+            {
+                Console.WriteLine($"Quotient: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Attempted division by 0 ends up here.");
+            }
+        }
+    }
+}

a3a530a0f7a97b57669a3055324fe2a52d9a677b
Naman <sharmanaman9561@gmail.com>
2025-06-04 14:45:19 +0530

    Pattern Matching Code

---
+++ NullCheck/NullCheck/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullCheck
+{
+    internal class Program
+    {
+        // Type Test
+        public static T MidPoint<T>(IEnumerable<T> sequence)
+        {
+            if (sequence is IList<T> list)
+            {
+                return list[list.Count / 2];
+            }
+            else if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence), "Sequence can't be null.");
+            }
+            else
+            {
+                int halfLength = sequence.Count() / 2 - 1;
+                if (halfLength < 0) halfLength = 0;
+                return sequence.Skip(halfLength).First();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            // Null Check
+            int? maybe = null;
+
+            if (maybe is int number)
+            {
+                Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+            }
+            else
+            {
+                Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+            }
+
+            List<int> numbers = new List<int> {1,2,3,4,5};
+            int middle = MidPoint(numbers);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RelationalPatterns/RelationalPatterns/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationalPatterns
+{
+    internal class Program
+    {
+        static string WaterState(int tempInFahrenheit)
+        {
+            if (tempInFahrenheit < 32)
+                return "solid";
+            else if (tempInFahrenheit == 32)
+                return "solid/liquid transition";
+            else if (tempInFahrenheit > 32 && tempInFahrenheit < 212)
+                return "liquid";
+            else if (tempInFahrenheit == 212)
+                return "liquid / gas transition";
+            else // tempInFahrenheit > 212
+                return "gas";
+        }
+        static void Main(string[] args)
+        {
+            int[] testTemperatures = { 20, 32, 50, 100, 212, 250 };
+
+            Console.WriteLine("Using WaterState:");
+            foreach (var temp in testTemperatures)
+            {
+                Console.WriteLine($"{temp}°F => {WaterState(temp)}");
+            }
+        }
+    }
+}

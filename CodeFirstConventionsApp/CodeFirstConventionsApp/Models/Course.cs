f4ff8a4b3b7879620f5a73f39cc8778346784e2e
Naman <sharmanaman9561@gmail.com>
2025-06-11 09:56:02 +0530

    #1956 Part 4 practice database creation and adding data to MySQL

--- CodeFirstConventionsApp/CodeFirstConventionsApp/Models/Course.cs
+++ CodeFirstConventionsApp/CodeFirstConventionsApp/Models/Course.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeFirstConventionsApp.Models
+﻿namespace CodeFirstConventionsApp.Models
 {
     public abstract class Course
     {

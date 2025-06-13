910409a87b01e4bd943a8c85053ae892befef6e2
Naman9561 <naman.sharma@entujn.com>
2025-06-13 11:24:05 +0530

    #1966/ Part-6 /indexer implementations and usage examples

---
+++ DayCollection/DayCollection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DayCollection
+{
+    internal class Program
+    {
+        string[] days = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
+        // Indexer with only a get accessor with the expression-bodied definition:
+        public int this[string day] => FindDayIndex(day);
+        private int FindDayIndex(string day)
+        {
+            for (int j = 0; j < days.Length; j++)
+            {
+                if (days[j] == day)
+                {
+                    return j;
+                }
+            }
+            throw new ArgumentOutOfRangeException(
+                nameof(day),
+                $"Day {day} is not supported.\nDay input must be in the form \"Sun\", \"Mon\", etc");
+        }
+        static void Main(string[] args)
+        {
+            var week = new Program();
+            Console.WriteLine(week["Fri"]);
+            try
+            {
+                Console.WriteLine(week["Made-up day"]);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Not supported input: {e.Message}");
+            }
+        }
+    }
+}
---
+++ TempRecord/TempRecord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Use indexers for encapsulating internal collections
+// and providing a more natural syntax for accessing elements.
+
+namespace TempRecord {
+    internal class Program {
+        // Array of temperature values
+        float[] temps =
+        {56.2F, 56.7F, 56.5F, 56.9F, 58.8F,61.3F, 65.9F, 62.1F, 59.2F, 57.5F};
+        // To enable client code to validate input
+        // when accessing your indexer.
+        public int Length => temps.Length;
+        // Indexer declaration.
+        // If index is out of range, the temps array will throw the exception.
+        public float this[int index]
+        {
+            get => temps[index];
+            set => temps[index] = value;
+        }
+        static void Main(string[] args)
+        {
+            var tempRecord = new Program();
+            // Use the indexer's set accessor
+            tempRecord[3] = 58.3F;
+            tempRecord[5] = 60.1F;
+            // Use the indexer's get accessor
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Element #{i} = {tempRecord[i]}");
+            }
+        }
+    }
+}

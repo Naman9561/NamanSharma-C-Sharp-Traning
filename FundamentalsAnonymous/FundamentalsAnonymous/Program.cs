533d720286526dd78f6268a8bb8ab3eaababf5da
Naman9561 <naman.sharma@entujn.com>
2025-06-13 15:57:20 +0530

    Fundamental Anonymous Code Practice

---
+++ FundamentalsAnonymous/FundamentalsAnonymous/Program.cs
@@ -0,0 +1,27 @@
+﻿class Product
+{
+    public string? Color { get; set; }
+    public decimal Price { get; set; }
+    public string? Name { get; set; }
+    public string? Category { get; set; }
+    public string? Size { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        List<Product> products = new()
+        {
+            new Product { Color = "Red", Price = 9.99m },
+            new Product { Color = "Blue", Price = 14.99m }
+        };
+
+        var productQuery = from prod in products
+                           select new { prod.Color, prod.Price };
+
+        foreach (var v in productQuery)
+        {
+            Console.WriteLine($"Color={v.Color}, Price={v.Price}");
+        }
+    }
+}

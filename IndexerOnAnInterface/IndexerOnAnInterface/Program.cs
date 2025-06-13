8090c1517e4695163d57fac33fab8d0db17e72ae
Naman9561 <naman.sharma@entujn.com>
2025-06-13 11:57:24 +0530

    #1966 / part-6 /indexer interface example with implementation and usage

---
+++ IndexerOnAnInterface/IndexerOnAnInterface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IndexerOnAnInterface
+{
+    public interface IIndexInterface
+    {
+        // Indexer declaration:
+        int this[int index]
+        {
+            get;
+            set;
+        }
+    }
+    class IndexerClass : IIndexInterface
+    {
+        private int[] arr = new int[100];
+        public int this[int index]   // Indexer implementation
+        {
+            get => arr[index];
+            set => arr[index] = value;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexerClass test = new IndexerClass();
+            Random rand = new Random();
+            // Initialize elements using indexer
+            for (int i = 0; i < 10; i++)
+            {
+                test[i] = rand.Next();
+            }
+            // Print elements using indexer
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Element #{i} = {test[i]}");
+            }
+        }
+    }
+}
